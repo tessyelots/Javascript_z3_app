ee71abe05abcd156b6018f69bc0ababed8bdb1f2
tessyelots <marek.smutny166@gmail.com>
2022-11-29 18:56:31 +0100

    xxx

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -29,19 +29,20 @@ function Main (props) {
                 redirect: 'follow',
                 referrerPolicy: 'no-referrer',
                 body: JSON.stringify({
+                    id: props.id,
                     date: getInput('datum-input'),
                     value: getInput('hodnota-input'),
                     typ: getRadio('typ'),
                     method: getRadio('metoda'),
                 })
             }).then(data => data.json()).then(data => {
-                console.log(data.message)
+                updateMerania(data.list);
             });
             setOpenAdd(false)
         }else{
             setOpenAdd(true)
-            fetch('http://localhost:8000/metoda',{
-                method: 'GET',
+            fetch('http://localhost:8000/getmetoda',{
+                method: 'POST',
                 mode: 'cors',
                 cache: 'no-cache',
                 credentials: 'same-origin',
@@ -50,12 +51,64 @@ function Main (props) {
                 },
                 redirect: 'follow',
                 referrerPolicy: 'no-referrer',
+                body: JSON.stringify({
+                    id: props.id,
+                })
             }).then(data => data.json()).then(data => {
                 console.log(data)
                 updateMetody(data.list)
             });
         }
     }
+
+    function getMerania(){
+        fetch('http://localhost:8000/getmeranie',{
+                method: 'POST',
+                mode: 'cors',
+                cache: 'no-cache',
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                redirect: 'follow',
+                referrerPolicy: 'no-referrer',
+                body: JSON.stringify({
+                    id: props.id,
+                })
+            }).then(data => data.json()).then(data => {
+                updateMerania(data.list)
+        });
+    }
+
+    function updateMerania(tableData){
+        var table = document.getElementById('merania-table');
+        const toDelete = document.querySelectorAll('.made');
+        toDelete.forEach(row => {
+            row.remove();
+          });
+      
+        for (let i = 0; i < tableData.length; i++){
+            var row = table.insertRow(i+1);
+            row.className = "made"
+            row.id = ""+(i+1)+""
+            
+            var cislo = row.insertCell(0);
+            cislo.innerHTML = ""+(i+1)+""
+            cislo.className = "made"
+            var datum = row.insertCell(1);
+            datum.innerHTML = tableData[i].datum.slice(0, 10)
+            datum.className = "made"
+            var hodnota = row.insertCell(2);
+            hodnota.innerHTML = tableData[i].hodnota
+            hodnota.className = "made"
+            var typ = row.insertCell(3);
+            typ.innerHTML = tableData[i].typ
+            typ.className = "made"
+            var metoda = row.insertCell(4);
+            metoda.innerHTML = tableData[i].metoda
+            metoda.className = "made"
+        }
+     }
 
     function updateMetody(list){
        const container = document.getElementById("metody-container");
@@ -90,6 +143,7 @@ function Main (props) {
                 redirect: 'follow',
                 referrerPolicy: 'no-referrer',
                 body: JSON.stringify({
+                    id: props.id,
                     nazov: getInput('nazov-input'),
                     popis: getInput('popis-input'),
                 })
@@ -99,8 +153,8 @@ function Main (props) {
             });
         }else{
             setOpenAdd(true)
-            fetch('http://localhost:8000/metoda',{
-                method: 'GET',
+            fetch('http://localhost:8000/getmetoda',{
+                method: 'POST',
                 mode: 'cors',
                 cache: 'no-cache',
                 credentials: 'same-origin',
@@ -109,6 +163,9 @@ function Main (props) {
                 },
                 redirect: 'follow',
                 referrerPolicy: 'no-referrer',
+                body: JSON.stringify({
+                    id: props.id,
+                })
             }).then(data => data.json()).then(data => {
                 console.log(data)
                 updateMetody(data.list)
@@ -128,11 +185,46 @@ function Main (props) {
                 redirect: 'follow',
                 referrerPolicy: 'no-referrer',
                 body: JSON.stringify({
+                    id: props.id,
                     nazov: getInput('zmazat-metodu-input'),
                 })
             }).then(data => data.json()).then(data => {
                 console.log(data)
                 updateMetody(data.list)
+            });
+    }
+
+    function getHodnoty(parent){
+        var list = [];
+        for (const child of parent.children) {
+            list.push(child.innerHTML);
+        }
+        return list;
+    }
+
+    function zmazatMeranie(){
+        var list = getHodnoty(document.getElementById(getInput("zmazat-meranie-input")));
+        console.log(list)
+        fetch('http://localhost:8000/delmeranie',{
+                method: 'POST',
+                mode: 'cors',
+                cache: 'no-cache',
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                redirect: 'follow',
+                referrerPolicy: 'no-referrer',
+                body: JSON.stringify({
+                    id: props.id,
+                    datum: list[1],
+                    hodnota: list[2],
+                    typ: list[3],
+                    metoda: list[4],
+                })
+            }).then(data => data.json()).then(data => {
+                console.log(data)
+                updateMerania(data.list)
             });
     }
 
@@ -181,6 +273,21 @@ function Main (props) {
                     <button onClick={zmazatMetodu}>ZMAZAT</button>
                     <br></br>
                     <button onClick={closeAdd}>CLOSE</button>
+                    <br></br>
+                    <h3>Tvoje merania</h3>
+                    <table id="merania-table" onClick={getMerania()}>
+                        <tr>
+                            <th>Cislo</th>
+                            <th>Date</th>
+                            <th>Hodnota</th>
+                            <th>Typ</th>
+                            <th>Metoda</th>
+                        </tr>
+                    </table>
+                    Napis cislo riadku merania, ktore chces vymazat: 
+                    <input id="zmazat-meranie-input"></input>
+                    <button onClick={zmazatMeranie}>ZMAZAT</button>
+                    <br></br>
                     <button onClick={click}>LOGOUT</button>
                 </div>
             );
@@ -190,6 +297,21 @@ function Main (props) {
                     <h1>MAIN PAGE id: {props.id}</h1>
                     <button onClick={click1}>Vlozit meranie</button>
                     <button onClick={click2}>Vlozit metodu</button>
+                    <br></br>
+                    <h3>Tvoje merania</h3>
+                    <table id="merania-table" onClick={getMerania()}>
+                        <tr>
+                            <th>Cislo</th>
+                            <th>Date</th>
+                            <th>Hodnota</th>
+                            <th>Typ</th>
+                            <th>Metoda</th>
+                        </tr>
+                    </table>
+                    Napis cislo riadku merania, ktore chces vymazat: 
+                    <input id="zmazat-meranie-input"></input>
+                    <button onClick={zmazatMeranie}>ZMAZAT</button>
+                    <br></br>
                     <button onClick={click}>LOGOUT</button>
                 </div>
             );
